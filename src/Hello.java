70157cde62e93c75cb40cf11a3e11afb19fd356e
netobjj <joseeuclides18@gmail.com>
2025-08-07 12:01:37 -0300

    feat: added method printInPortuguese()

--- src/Hello.java
+++ src/Hello.java
@@ -3,4 +3,8 @@ public class Hello {
     public static void print() {
         System.out.println("Hello World!");
     }
+
+    public static void printInPortuguese() {
+        System.out.println("Olá Mundo!");
+    }
 }
